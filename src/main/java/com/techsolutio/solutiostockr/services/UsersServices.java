2cbf91d1a2b055279a5aad6bd13c1401f5f832dd
Felipe Holanda <felipe.nether@icloud.com>
2023-09-01 18:21:07 -0300

    feat: finished users features

---
+++ src/main/java/com/techsolutio/solutiostockr/services/UsersServices.java
@@ -0,0 +1,23 @@
+package com.techsolutio.solutiostockr.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.techsolutio.solutiostockr.dto.UsersDto;
+import com.techsolutio.solutiostockr.models.Users;
+
+public interface UsersServices {
+
+    public Users createUser(UsersDto userData);
+
+    public List<Users> listUsers();
+
+    public Users retrieveUser(UUID id);
+
+    public Users updateUser(UUID id, UsersDto userData);
+
+    public void deleteUser(UUID id);
+
+    public Users togglePrivileges(UUID id);
+
+}

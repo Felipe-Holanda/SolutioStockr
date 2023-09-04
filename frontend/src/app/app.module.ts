812309554b006a96a5512d930c332d8c02beb9d4
Felipe Holanda <felipe.nether@icloud.com>
2023-09-04 06:31:06 -0300

    feat: added more pages and elements

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SmartHeaderComponent } from './components/smart-header/smart-header.component';
@@ -8,6 +7,8 @@ import { ProductsComponent } from './components/products/products.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProductModalComponent } from './components/product-modal/product-modal.component';
 import { ProductListerComponent } from './components/product-lister/product-lister.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +17,13 @@ import { ProductListerComponent } from './components/product-lister/product-list
     ProductsComponent,
     LoginComponent,
     ProductModalComponent,
-    ProductListerComponent
+    ProductListerComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { SmartHeaderComponent } from 'src/app/components/smart-header/smart-header.component';
+import { ProductListerComponent } from 'src/app/components/product-lister/product-lister.component';
+import { ProductModalComponent } from 'src/app/components/product-modal/product-modal.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent {
+
+  constructor() {}
+
 
 }

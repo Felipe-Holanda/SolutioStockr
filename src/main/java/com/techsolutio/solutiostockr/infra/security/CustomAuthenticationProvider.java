c3a50cda2fe5bc981ab1c9dff09b5433626d9f86
Felipe Holanda <felipe.nether@icloud.com>
2023-09-04 01:25:28 -0300

    feat: added a custom auth provider to manage the authentication

---
+++ src/main/java/com/techsolutio/solutiostockr/infra/security/CustomAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package com.techsolutio.solutiostockr.infra.security;
+
+import com.techsolutio.solutiostockr.exceptions.AppException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.techsolutio.solutiostockr.repositories.UserRepository;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    String username = authentication.getName();
+    String password = authentication.getCredentials().toString();
+
+    UserDetails user = userRepository.findByLogin(username);
+
+    if (user == null) {
+        throw new AppException("Wrong user or password", HttpStatus.UNAUTHORIZED);
+    }
+
+    // Use o BCrypt para verificar a senha
+    if (!passwordEncoder.matches(password, user.getPassword())) {
+        throw new AppException("Wrong user or password", HttpStatus.UNAUTHORIZED);
+    }
+
+    return new UsernamePasswordAuthenticationToken(user, password, user.getAuthorities());
+}
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
